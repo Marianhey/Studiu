a694d1a377eed28e4b4fa4be05744e82d8317e39
Marianhey <foiumarian@gmail.com>
2024-11-13 09:26:47 +0200

    actualizare

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-const options={}
+const correctUsername = "utilizator";
+        const correctPassword = "parola123";
+
+        function login() {
+            const username = document.getElementById("username").value;
+            const password = document.getElementById("password").value;
+            const errorMessage = document.getElementById("error-message");
+
+            if (username === correctUsername && password === correctPassword) {
+                alert("Autentificare reușită!");
+                errorMessage.style.display = "none";
+                
+                window.location.href = "pagina_principala.html";
+            } else {
+                errorMessage.style.display = "block";
+            }
+        }
